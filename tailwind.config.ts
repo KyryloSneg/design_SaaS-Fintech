3b1a0e6f4beaec9a8b2d37fdfad7f81a985c0e6a
KyryloSneg <podhvatilinkirill511@gmail.com>
2026-03-22 14:48:44 +0200

    feat: initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,11 @@
+import type { Config } from "tailwindcss";
+
+const config = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  plugins: [],
+} satisfies Config;
+
+export default config;
